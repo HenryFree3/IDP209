7f97e703268a0c4466beea0077c797cfe65e31c8
Katrina Tse <katrinatse2002@gmail.com>
2022-02-28 13:35:32 +0000

    ultrasonic sensor test completed with distance calculation

--- Sensor tests/ultrasonic_sensor_test.ino
+++ Sensor tests/ultrasonic_sensor_test.ino
@@ -1,12 +1,16 @@
-// pulse generation of 10 microseconds
-// 60 ms between pulses
+/* 
+    Pulse generation of 10 microseconds
+    60 ms between pulses 
+*/
 
-int echo = 8; // set output pin
-int trigger = 7; // set input pin
+const int echo = 8; // set input pin
+const int trigger = 7; // set output pin
 
 unsigned long duration = 0;
 unsigned long timeout = 2500000;
 int val;
+int dist;
+int distance;
 
 void setup() {
     Serial.begin(9600);
@@ -21,23 +25,26 @@ void loop() {
     Serial.println("Alive");
     pulsegeneration();
     duration = pulseIn(echo, HIGH);
+    distance = distancecalculation();
     Serial.println("Pulse duration is");
     Serial.println(duration);
-    delay(60);
+    Serial.println("Distance in cm is");
+    Serial.println(distance);
 
 }
 
 void pulsegeneration() {    
   
     Serial.println("Sending pulse");
+    delay(60);
     digitalWrite(trigger, LOW);
     digitalWrite(trigger, HIGH);
-    Serial.println("pulse high");
-    Serial.println(digitalRead(trigger));
+    //Serial.println("pulse high");
+    //Serial.println(digitalRead(trigger));
     delayMicroseconds(15);
     digitalWrite(trigger, LOW);
-    Serial.println("pulse low");
-    Serial.println(digitalRead(trigger));
+    //Serial.println("pulse low");
+    //Serial.println(digitalRead(trigger));
 
 }
 
@@ -47,4 +54,13 @@ void pintesting() {
     val = digitalRead(echo);
     Serial.println(val);
     delay(1000);
+
+}
+
+void distancecalculation() {
+    
+    // returns distance in cm
+    
+    dist = duration / 58
+
 }
