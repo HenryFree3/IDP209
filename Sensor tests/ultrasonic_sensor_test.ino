6f2e23024b6be422f0d908d87f3450b191830eef
Katrina Tse <katrinatse2002@gmail.com>
2022-02-28 12:25:39 +0000

    Ultrasonic sensor testing ok without distance calculation

--- Sensor tests/ultrasonic_sensor_test.ino
+++ Sensor tests/ultrasonic_sensor_test.ino
@@ -1,11 +1,12 @@
 // pulse generation of 10 microseconds
 // 60 ms between pulses
 
-int echo = 4; // set output pin
-int trigger = 3; // set input pin
+int echo = 8; // set output pin
+int trigger = 7; // set input pin
 
-int duration = 0;
-int inPin_stat = 0;
+unsigned long duration = 0;
+unsigned long timeout = 2500000;
+int val;
 
 void setup() {
     Serial.begin(9600);
@@ -16,17 +17,20 @@ void setup() {
 }
 
 void loop() {
-  
+
     Serial.println("Alive");
     pulsegeneration();
     duration = pulseIn(echo, HIGH);
     Serial.println("Pulse duration is");
     Serial.println(duration);
+    delay(60);
 
 }
 
-void pulsegeneration() {  
+void pulsegeneration() {    
+  
     Serial.println("Sending pulse");
+    digitalWrite(trigger, LOW);
     digitalWrite(trigger, HIGH);
     Serial.println("pulse high");
     Serial.println(digitalRead(trigger));
@@ -34,6 +38,13 @@ void pulsegeneration() {
     digitalWrite(trigger, LOW);
     Serial.println("pulse low");
     Serial.println(digitalRead(trigger));
-    delay(60);
 
+}
+
+void pintesting() {
+  
+    digitalWrite(echo, HIGH);
+    val = digitalRead(echo);
+    Serial.println(val);
+    delay(1000);
 }
