ed61259a7a767ddc9357b56f8d64321ae016d8cf
Katrina Tse <katrinatse2002@gmail.com>
2022-03-01 11:00:42 +0000

    Updated colour sensor test code to match circuit

--- Sensor tests/colour_sensor_test.ino
+++ Sensor tests/colour_sensor_test.ino
@@ -1,25 +1,22 @@
-int i;
-int pin;
-
-pin = //insert pin number
+const int pin = ;//insert pin number
 
 void setup() {
     pinMode(pin, INPUT);
-    Serial.begin(9600)
+    Serial.begin(9600);
 }
 
 void loop() {
-    if (digitalRead(pin) == LOW) {
-        Serial.println('Detected red');
+    if (digitalRead(pin) == HIGH) {
+        Serial.println("Detected red");
         delay (500);
     }
 
-    else if (digitalRead(pin) == HIGH) {
-        Serial.println('Detected blue');
+    else if (digitalRead(pin) == LOW) {
+        Serial.println("Detected blue");
         delay (500);
     }
     else {
-        Serial.println('No reading')
+        Serial.println("No reading");
     }
 
 }
