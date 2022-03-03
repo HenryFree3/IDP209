9862ace90adaa871cdc1283d98aacc45a8e80ead
Katrina Tse <katrinatse2002@gmail.com>
2022-03-03 16:58:38 +0000

    Work in progress, dealing with sensor issues

--- block_searching.ino
+++ block_searching.ino
@@ -4,6 +4,7 @@ ultrasonic scan until return reading != 0 then calculate distance and go that wa
 // GO SLOW!!!
 
 #include <Adafruit_MotorShield.h>
+#include <Arduino_LSM6DS3.h>
 
 const int echoPin = 8; // set ultrasonic output pin
 const int triggerPin = 7; // set ultrasonic input pin
@@ -30,7 +31,7 @@ WiFiServer server(23);
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
-// Configure all 4 motors outputs. M1 is back left, M2 is back right, M3 is front left, M4 is front right.
+// Configure all 4 motors outputs. M3 is front left, M4 is front right.
 Adafruit_DCMotor *M1 = AFMS.getMotor(1);
 Adafruit_DCMotor *M2 = AFMS.getMotor(2);
 Adafruit_DCMotor *M3 = AFMS.getMotor(3);
@@ -41,10 +42,17 @@ void setup () {
 
     Serial.begin(9600);
 
+    // Initialise IMU
+    IMU.begin()
+    Serial.println("IMU initialised")
+
     pinMode(triggerPin, OUTPUT);
     pinMode(echoPin, INPUT);
     pinMode(leftfrontPin, INPUT);
     pinMode(rightfrontPin, INPUT);
+    Serial.println("Pins initialised")
+
+    Serial.println("Setup complete")
 
 }
 
@@ -81,11 +89,12 @@ void detect_block() {
 
         if (count == 2) {
             // robot needs to pivot around cross and spin (limited to range of the box), use rotary encoder to track heading
+            // use built in gyro?
             // check edge reading of ultrasonic sensor
             pulsegeneration();
             duration = pulseIn(echoPin, HIGH);
 
-            if (duration != 0) {
+            if (duration < ) {
                 distance = distancecalculation();
                 // match distance to number of rotations on rotary encoder
             }
@@ -95,16 +104,24 @@ void detect_block() {
     }
 }
 
+// For bot moving functions, might be better to code for distance rather than time
+
 void go_forward() {
 
     Motors[2].run(FORWARD);
     Motors[3].run(FORWARD);
+    delay(100);
+    Motors[2].release();
+    Motors[3].release();
 }
 
 void go_backward() {
 
     Motors[2].run(BACKWARD);
     Motors[3].run(BACKWARD);
+    delay(100);
+    Motors[2].release();
+    Motors[3].release();
 
 }
 
@@ -112,11 +129,25 @@ void turn_left() {
 
     Motors[2].run(FORWARD);
     Motors[3].run(BACKWARD);
+    delay(100);
+    Motors[2].release();
+    Motors[3].release();
 }
 
 void turn_right() {
 
     Motors[2].run(BACKWARD);
     Motors[3].run(FORWARD);
+    delay(100);
+    Motors[2].release();
+    Motors[3].release();
+
+}
+
+float readGyro() {
+
+}
+
+int colour_detection() {
 
 }
