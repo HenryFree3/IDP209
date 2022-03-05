4ea2eabbf0ee9b89d138271f8d348bb4bfab12d8
Henry Free <hf360@cam.ac.uk>
2022-03-05 01:12:22 +0000

    Update to encoder test - readability

--- Unit tests/encoder_test.ino
+++ Unit tests/encoder_test.ino
@@ -1,5 +1,10 @@
 /*
   Optical encoder test
+
+  Next step: PID control for matching encoder counts
+    i.e. when trying to move forwards, drive the difference
+    between the number of encoder steps on the wheels to zero
+    by adjusting the motor speeds.
  */
 
 #include <Arduino.h>
@@ -8,65 +13,75 @@
 #include <Adafruit_MotorShield.h>
 #include "IDPLib.h"
 
-int step = 50; // Milliseconds between inputs
-int speed1 = 230; // Motor speed, 0-255, for forwards
-int speed2 = 115; // Reduced motor speed, for one side when turning
-
-int double_count = 0;
-
-float count = 0;
+// Wheel geometry
 const int slots = 36;
 const float degPerSlot = 360/slots;
 float velocity;
 float diameter = 100; // Wheel diameter, mm
+float wheelSep = 220; // Separation between two wheels, mm
+// The above can be used, with two wheels velocities, to
+// determine the instantaneous centre of rotation
 
-float velArray[slots];
+int counts[100];
 float sum = 0;
 int i = 0;
+int j = 0;
 
-IDPLib IDP;
+bool encoder;
+bool encoderOld;
+
+int leftEncoderPin = 5;
+int rightEncoderPin = 7;
+float pollRate = 1000.0;
+float period = 1/pollRate;
 
 volatile unsigned long time;
 volatile unsigned long timeOld;
 bool updated = false;
 
-void setup() {
+IDPLib IDP;
+
+// ISR cannot be declared inside a class member
+ISR(TCB0_INT_vect) {
+  encoder = digitalRead(7);
+  IDP.test = true;
+  TCB0.INTFLAGS = TCB_CAPT_bm;
+}
+
+void setup() { 
   IDP.motorStart();
+  IDP.encoderStart(leftEncoderPin, rightEncoderPin, pollRate);
+  encoder = encoderOld = digitalRead(rightEncoderPin);
+  
   IDP.motors[3].run(FORWARD);
-  IDP.motors[3].setSpeed(30);
+  IDP.motors[3].setSpeed(60);
+
   Serial.begin(9600);
-  pinMode(6, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
   Serial.println("Ready");
-  attachInterrupt(7, encoderChange, CHANGE);
-  timeOld = 0;
 }
 
 void loop() {
-  if (updated) {
-    velocity = 3.14159*diameter/(slots*(time-timeOld)*1E-6);
-    timeOld = time;
-    Serial.print("Velocity is: ");
-    Serial.print(velocity);
-    Serial.println(" mm/s");
-    velArray[i] = velocity;
+  if (IDP.test) {
     i++;
-    updated = false;
-
-    if (i % (slots-1) == 0) {
+    if (encoder != encoderOld) {
+      Serial.println("Encoder move detected after " + String(i) + " polls");
+      counts[j] = i;
+      encoderOld = encoder;
       i = 0;
-      for (int j = 0; j <= slots - 1; j++) {
-        sum = sum + velArray[j];
-      }
-      Serial.print("Avg. vel over last revolution was: ");
-      Serial.print(float(sum/slots));
-      Serial.println(" mm/s");
-      sum = 0;
+      j++;    
     }
+    IDP.test = false;
   }
-}
-
-void encoderChange() {
-  time = micros();
-  updated = true;
+  
+  if (j % (sizeof(counts)/sizeof(counts)[0]) == 0) {
+    // Determine average time between polls from last readings.
+    // Useful for calibrating motor speed output against speed control input.
+        j = 0;
+        for (int k = 0; k < (sizeof(counts)/sizeof(counts)[0]); k++) {
+          sum = sum + counts[k];
+        }
+        Serial.print("Avg. polls per slot was: ");
+        Serial.println(float(sum/(sizeof(counts)/sizeof(counts)[0])));
+        sum = 0;
+      }
 }
